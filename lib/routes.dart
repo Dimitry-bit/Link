417d356995e523d8847855bb82d76ff010e38c70
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-06-13 02:48:03 +0300

    feat: add routes

---
+++ lib/routes.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String dashboardPage = '/';
+  static const String gpaPage = '/gpa';
+  static const String mapPage = '/map';
+  static const String viewSchedulesPage = '/schedules';
+
+  static const String coursesPage = '/courses';
+  static const String locationsPage = '/locations';
+  static const String personnelPage = '/personnel';
+  static const String manageSchedulesPage = '/schedules_editor';
+
+  static const String settingsPage = '/settings';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case dashboardPage:
+      case gpaPage:
+      case mapPage:
+      case viewSchedulesPage:
+      case coursesPage:
+      case locationsPage:
+      case personnelPage:
+      case manageSchedulesPage:
+      case settingsPage:
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => Placeholder(
+              color: Color((Random().nextDouble() * 0xFFFFFF).toInt())
+                  .withOpacity(1.0)),
+        );
+      default:
+        throw const RouteException("Route not found");
+    }
+  }
+
+  static List<Route> generateInitialRoutes(String name) {
+    return <Route>[MaterialPageRoute(builder: (_) => const Placeholder())];
+  }
+}
+
+class RouteException implements Exception {
+  final String message;
+  const RouteException(this.message);
+}
