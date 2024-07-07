5a616b98642c644c106c8952c7491d6167a3a503
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-07-07 16:35:16 +0300

    doc: document response class

--- lib/controllers/response.dart
+++ lib/controllers/response.dart
@@ -1,7 +1,16 @@
+/// Represents a response object that encapsulates either a data payload
+/// or an error message.
 class Response<T> {
+  /// The data payload of this response.
   T? data;
+
+  /// The error message string if an operation resulted in an error.
   String errorStr;
 
   Response(this.data, {this.errorStr = ''});
+
+  /// Constructs an error [Response] object with the specified [errorMessage].
+  ///
+  /// This is a shorthand constructor for creating a response indicating failure.
   Response.error(String errorMessage) : this(null, errorStr: errorMessage);
 }
