c5c7fa8895e03a2f8d99b23b97e405bd8a727054
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-07-07 16:36:25 +0300

    refactor: Make Response class immutabile

--- lib/controllers/response.dart
+++ lib/controllers/response.dart
@@ -2,15 +2,15 @@
 /// or an error message.
 class Response<T> {
   /// The data payload of this response.
-  T? data;
+  final T? data;
 
   /// The error message string if an operation resulted in an error.
-  String errorStr;
+  final String errorStr;
 
-  Response(this.data, {this.errorStr = ''});
+  const Response(this.data, {this.errorStr = ''});
 
   /// Constructs an error [Response] object with the specified [errorMessage].
   ///
   /// This is a shorthand constructor for creating a response indicating failure.
-  Response.error(String errorMessage) : this(null, errorStr: errorMessage);
+  const Response.error(String errorMessage) : this(null, errorStr: errorMessage);
 }
