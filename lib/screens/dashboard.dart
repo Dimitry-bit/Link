be30676bc8a284ae061fecc3574910299e8809fd
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-07-13 17:09:47 +0300

    fix: dashboard not rendering in release build

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -12,36 +12,34 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     final userSettings = Provider.of<UserSettings>(context);
 
-    return Flexible(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const PageHeader(title: 'Dashboard'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const PageHeader(title: 'Links'),
-                        CommunityLinks(links: userSettings.communityLinks()),
-                      ],
-                    ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const PageHeader(title: 'Dashboard'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const PageHeader(title: 'Links'),
+                      CommunityLinks(links: userSettings.communityLinks()),
+                    ],
                   ),
                 ),
               ),
-              const GPAGauge(),
-            ],
-          ),
-        ],
-      ),
+            ),
+            const GPAGauge(),
+          ],
+        ),
+      ],
     );
   }
 }
